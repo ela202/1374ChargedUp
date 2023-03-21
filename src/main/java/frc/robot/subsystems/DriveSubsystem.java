2d2dccdf244d98c28f9ea3b2275b3a4b2d148585
ela202 <1aydinerela@hdsb.ca>
2023-03-21 18:18:40 -0400

    updating drive subsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,160 +1,94 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
 
-import frc.robot.OI;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
-// import edu.wpi.first.wpilibj.motorcontrol.Talon;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-
-
-/**
- * Add your docs here.
- */
 public class DriveSubsystem extends SubsystemBase {
+  /** Creates a new DriveSubsystem. */
+    CANSparkMax L1motor;
+    RelativeEncoder L1encoder;
+    SparkMaxPIDController L1controller;
 
-  AHRS ahrs = new AHRS(SPI.Port.kMXP);
+    CANSparkMax L2motor;
+    RelativeEncoder L2encoder;
+    SparkMaxPIDController L2controller;
 
+    CANSparkMax R1motor;
+    RelativeEncoder R1encoder;
+    SparkMaxPIDController R1controller;
 
-  // TalonSRX Left1 = new TalonSRX(RobotMap.Left1);
-  // TalonSRX Left2 = new TalonSRX(RobotMap.Left2);
-  // TalonSRX Right1 = new TalonSRX(RobotMap.Right1);
-  // TalonSRX Right2 = new TalonSRX(RobotMap.Right2);
+    CANSparkMax R2motor;
+    RelativeEncoder R2encoder;
+    SparkMaxPIDController R2controller;
 
-  // Talon Left1 = new Talon(RobotMap.Left1);
-  // Talon Left2 = new Talon(RobotMap.Left2);
-  // Talon Right1 = new Talon(RobotMap.Right1);
-  // Talon Right2 = new Talon(RobotMap.Right2); 
-  
-  CANSparkMax Left1;
-  CANSparkMax Left2;
-  CANSparkMax Right1;
-  CANSparkMax Right2;
-  RelativeEncoder driveEncoder;
- 
   public DriveSubsystem() {
 
-    Left1 = new CANSparkMax(RobotMap.Left1, MotorType.kBrushless);
-    Left2 = new CANSparkMax(RobotMap.Left2, MotorType.kBrushless);
-    Right1 = new CANSparkMax(RobotMap.Right1, MotorType.kBrushless);
-    Right2 = new CANSparkMax(RobotMap.Right2, MotorType.kBrushless);
+    double rampValue = 0.3; // Set the time in seconds for ramp up value (acceleration curve)
 
-    driveEncoder = Left1.getEncoder();
 
-    Right1.setInverted(true);
-    Right2.setInverted(true);
-  }
-
-  public void TankDrive (double left, double right) {
-
-    double yaw = ahrs.getAngle();
-    double pitch = ahrs.getPitch();
-    double roll = ahrs.getRoll(); 
-    double threshold = 10.0; // adjust as needed
-    
-    while (balanceEnabled()) {
-      if (Math.abs(pitch) > threshold) {
-        SmartDashboard.putBoolean("auto balance enabled", true);
-        // If the pitch angle exceeds the threshold, reverse the direction of the motors
-        double pitchRadian = pitch * (Math.PI / 180.0);
-        left = Math.sin(pitchRadian) * -1;
-        right = Math.sin(pitchRadian) * -1;
-      }
-    }
+    L1motor = new CANSparkMax(RobotMap.Left1, MotorType.kBrushless);
+    L1encoder = L1motor.getEncoder();
+    L1controller = L1motor.getPIDController();
+    L1motor.setOpenLoopRampRate(rampValue);
+    L1motor.burnFlash();
     
 
-    SmartDashboard.putNumber("pitch", pitch);
-    SmartDashboard.putNumber("roll", roll);
-    SmartDashboard.putNumber("yaw", yaw);
+    L2motor = new CANSparkMax(RobotMap.Left2, MotorType.kBrushless);
+    L2encoder = L2motor.getEncoder();
+    L2controller = L2motor.getPIDController();
+    L2motor.setOpenLoopRampRate(rampValue);
+    L2motor.burnFlash();
+    
 
-    // Drive the left and right sides of the neos
-    Left1.set(left);
-    Left2.set(left);
-    Right1.set(right);
-    Right2.set(right);
+    R1motor = new CANSparkMax(RobotMap.Right1, MotorType.kBrushless);
+    R1encoder = R1motor.getEncoder();
+    R1controller = R1motor.getPIDController();
+    R1motor.setOpenLoopRampRate(rampValue);
+    R1motor.burnFlash();
 
-    // Left1.set(ControlMode.PercentOutput, left);
-    // Left2.set(ControlMode.PercentOutput, left);
-    // Right1.set(ControlMode.PercentOutput,-right);
-    // Right2.set(ControlMode.PercentOutput,-right);
-               
+    R2motor = new CANSparkMax(RobotMap.Right2, MotorType.kBrushless);
+    R2encoder = R2motor.getEncoder();
+    R2controller = R2motor.getPIDController();
+    R2motor.setOpenLoopRampRate(rampValue);
+    R2motor.burnFlash();
   }
 
-
-  public void ArcadeDrive (double speed, double turn) {
-    TankDrive((speed - turn) * 0.5, (speed + turn) * 0.5);
+  public void TankDrive(double left, double right) {
+    L1motor.set(-left);
+    L2motor.set(-left);
+    R1motor.set(right);
+    R2motor.set(right);
   }
 
-  public boolean balanceEnabled () {
-    if (OI.getAutoBalance()) {
-      return true;
+  public double speedRamp(double speed){
+    //return 0.2*(Math.pow(speed, 3))+0.8*(Math.pow(speed, 2));
+    
+    
+    if (speed > 0){
+      return 0.2*(Math.pow(speed, 3))+0.8*(Math.pow(speed, 2));
+
     } else {
-      return false;
-    }
-  }
-
-  public void AutoTankDrive(double left, double right) {
-
-    double yaw = ahrs.getAngle();
-    double pitch = ahrs.getPitch();
-    double roll = ahrs.getRoll(); 
-    double threshold = 10.0; // adjust as needed
+      speed = speed * -1;
+    } return   -1 * (0.2*(Math.pow(speed, 3))+0.8*(Math.pow(speed, 2)));
     
-    if (Math.abs(pitch) > threshold) {
-      // If the pitch angle exceeds the threshold, reverse the direction of the motors
-      double pitchRadian = pitch * (Math.PI / 180.0);
-      left = Math.sin(pitchRadian) * -1;
-      right = Math.sin(pitchRadian) * -1;
-    }
-    
-    SmartDashboard.putNumber("pitch", pitch);
-    SmartDashboard.putNumber("roll", roll);
-    SmartDashboard.putNumber("yaw", yaw);
-
-    // Drive the left and right sides of the neos
-    Left1.set(left);
-    Left2.set(left);
-    Right1.set(right);
-    Right2.set(right);
-
-    // Left1.set(ControlMode.PercentOutput, left);
-    // Left2.set(ControlMode.PercentOutput, left);
-    // Right1.set(ControlMode.PercentOutput,-right);
-    // Right2.set(ControlMode.PercentOutput,-right);               
+  }
+  
+  
+  public void ArcadeDrive(double speed, double turn) {  
+    speed = speedRamp(speed);
+    turn = speedRamp(turn);
+    turn = turn * 0.75;
+    TankDrive((speed) - turn, speed + turn);
   }
 
-  public void AutoArcadeDrive (double speed, double turn) {
-    TankDrive((speed - turn) * 0.5, (speed + turn) * 0.5);
-  }
-
-  public void driveReset(){
-    AutoArcadeDrive(0, 0);
-    driveEncoder.setPosition(0);
-  }
-
-  public double encoderPosition() {
-    return driveEncoder.getPosition();
-  }
 
 }
-
-
- 
-
